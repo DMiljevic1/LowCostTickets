537d9e483b069910f584e8da235ff9c2d4b4e76d
unknown <duje.miljevic@gmail.com>
2024-05-18 16:35:50 +0200

    added docker compose

---
+++ King.Tickets.Infrastructure/Migrations/20240518113546_InitialMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace King.Tickets.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArrivalAirport = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartureAirport = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ArrivalDepartureDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NumberOfPassengers = table.Column<int>(type: "int", nullable: false),
+                    TotalPrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
