d47d35ed30c011f82c13bf6ba2cc675289f4a555
unknown <duje.miljevic@gmail.com>
2024-06-02 18:38:04 +0200

    added validation for airport codes

--- King.Tickets.Application/Validation/TicketFilterValidator.cs
+++ King.Tickets.Application/Validation/TicketFilterValidator.cs
@@ -6,6 +6,7 @@ namespace King.Tickets.Application.Validation;
 
 public class TicketFilterValidator : AbstractValidator<TicketFilterDto>
 {
+	private const int AirportCodeLenght = 3;
 	public TicketFilterValidator()
 	{
 		RuleFor(filter => filter.DepartureAirport).NotEmpty().WithMessage("Departure airport cannot be empty!");
@@ -15,7 +16,9 @@ public class TicketFilterValidator : AbstractValidator<TicketFilterDto>
 		RuleFor(filter => filter.NumberOfPassengers).GreaterThan(0).WithMessage("Number of passengers must be greater than zero!");
 		RuleFor(filter => filter).Must(IsDepartureDateBeforeReturnDate).WithMessage("Departure date must be before return date!");
 		RuleFor(filter => filter.DepartureDate).Must(IsValid).WithMessage("Invalid departure date!");
-	}
+		RuleFor(filter => filter.DepartureAirport).Must(IsValidLenght).WithMessage("Departure airport code must have 3 characters!");
+        RuleFor(filter => filter.ArrivalAirport).Must(IsValidLenght).WithMessage("Arrival airport code must have 3 characters!");
+    }
 
 	private bool IsCurrencyValid(Currency? currency)
 	{
@@ -38,5 +41,9 @@ public class TicketFilterValidator : AbstractValidator<TicketFilterDto>
 	private bool IsValid(DateTime departureDate)
 	{
 		return (departureDate.Date >= DateTime.Today)  ? true : false;
+	}
+	private bool IsValidLenght(string airportCode)
+	{
+		return (airportCode.Length == AirportCodeLenght) ? true : false;
 	}
 }
